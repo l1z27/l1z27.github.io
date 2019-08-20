131a0d3166f98972db7f51c9cc4550624bf2260d
liz <velasquezliz27@gmail.com>
2019-08-19 21:03:22 -0700

    init setup site

---
+++ includes/js/main.js
@@ -0,0 +1,9 @@
+//Main js 
+$.ajax({
+    url: "https://api-beta.open5e.com/spells/",
+    success: function (data) {
+        //data is an array object not text
+
+    },
+    dataType: "html"
+});
