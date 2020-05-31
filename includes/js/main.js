206de1a61a9e38186037dcd895912fc6f0b8c3ae
liz <velasquezliz27@gmail.com>
2020-05-30 18:05:36 -0700

    test text

--- includes/js/main.js
+++ includes/js/main.js
@@ -1,9 +1,7 @@
 //Main js 
-$.ajax({
-    url: "https://api-beta.open5e.com/spells/",
-    success: function (data) {
-        //data is an array object not text
-
-    },
-    dataType: "html"
+$(document).ready(function(){
+    // $.getJSON('https://api-beta.open5e.com/spells/?format=json', function(data){
+        
+    //     console.log(data.results);
+    // });
 });
